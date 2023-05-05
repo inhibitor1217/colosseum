dddd543a92aa31d00e9268e6f29f4dafd6f98bbe
inhibitor1217 <inhibitor@kaist.ac.kr>
2023-05-05 14:25:07 +0900

    feat(apps/github-webhook): setup api-gw and lambda handler with serverless framework

--- apps/github-webhook/src/main.ts
+++ apps/github-webhook/src/main.ts
@@ -1,2 +1,14 @@
-// TODO
-export const handler = () => {};
+import { type APIGatewayProxyHandlerV2 } from "aws-lambda";
+
+export const webhook: APIGatewayProxyHandlerV2 = async (event, ctx) => {
+  console.log(event);
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      msg: "ok",
+    }),
+  };
+};
