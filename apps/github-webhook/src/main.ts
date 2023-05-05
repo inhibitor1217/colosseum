8e79ea6bb23048e0a5cf531322fcf366a451e6fd
inhibitor1217 <inhibitor@kaist.ac.kr>
2023-05-05 22:22:28 +0900

    feat(apps/github-webhook): setup firestore

--- apps/github-webhook/src/main.ts
+++ apps/github-webhook/src/main.ts
@@ -1,4 +1,11 @@
 import { type APIGatewayProxyHandlerV2 } from "aws-lambda";
+import { initializeApp as initializeFirebaseApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+
+import { firebaseOptions } from "./config/firebase";
+
+const firebase = initializeFirebaseApp(firebaseOptions);
+const firestore = getFirestore(firebase);
 
 export const webhook: APIGatewayProxyHandlerV2 = async (event, ctx) => {
   console.log(event);
